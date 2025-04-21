f309751bd233633efb990f85532eecb3a205741f
Igor Clauss <mail@igorclauss.de>
2025-04-21 13:47:05 +0200

    Update take-picture-field.php
    Added German Translations

--- resources/lang/en/take-picture-field.php
+++ resources/lang/en/take-picture-field.php
@@ -1,6 +1,12 @@
 <?php
 
-// future translations for emmanpbarrameda/FilamentTakePictureField
+declare(strict_types=1);
+
 return [
-    //
+    'Take Photo' => 'Take Photo',
+    'Click to capture a new photo' => 'Click to capture a new photo',
+    'Try Again' => 'Try Again',
+    'Flip Camera' => 'Flip Camera',
+    'Select Camera' => 'Select Camera',
+    'Cancel' => 'Cancel',
 ];
